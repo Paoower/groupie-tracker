37d50303b26b799b166bc411e4000d917d77fdcb
mtrebert <maxime.trebert@gmail.com>
2024-07-29 12:17:15 +0200

    V2

--- main.go
+++ main.go
@@ -5,42 +5,57 @@ import (
 	"groupie-tracker/utils"
 	"log"
 	"net/http"
+	"os"
 )
 
-func main() {
+func init() {
 	log.SetFlags(log.Ltime)
 	log.SetPrefix("groupie-tracker:")
+	getData()
+}
 
-	go getData()
+func Handler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/searchbar":
+		handlers.SearchBarHandler(w, r)
+	case "/filters":
+		handlers.FiltersHandler(w, r)
+	case "/artists":
+		handlers.ArtistsHandler(w, r)
+	case "/artist":
+		handlers.ArtistHandler(w, r)
+	case "/":
+		handlers.HomeHandler(w, r)
+	default:
+		http.NotFound(w, r)
+	}
+}
 
-	scripts := http.FileServer(http.Dir("./templates/scripts"))
-	http.Handle("/scripts/", http.StripPrefix("/scripts/", scripts))
-
-	http.HandleFunc("/searchbar", handlers.SearchBarHandler)
-	http.HandleFunc("/filters", handlers.FiltersHandler)
-	http.HandleFunc("/artists", handlers.ArtistsHandler)
-	http.HandleFunc("/artist", handlers.ArtistHandler)
-	http.HandleFunc("/", handlers.HomeHandler)
-
-	log.Println(utils.GREEN, "Server started at http://localhost:8080", utils.NONE)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	http.HandleFunc("/", Handler)
+	log.Printf("Server starting on port %s", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
 func getData() {
 	err := utils.ReadJson("json/artists.json", &utils.ArtistsJson)
 	if err != nil {
-		log.Fatal(err)
+		log.Printf("Error loading artists.json: %v", err)
 	}
 
 	err = utils.ReadJson("json/locations.json", &utils.LocationsJson)
 	if err != nil {
-		log.Fatal(utils.RED, err)
+		log.Printf("Error loading locations.json: %v", err)
 	}
 
 	err = utils.ReadJson("json/relation.json", &utils.RelationJson)
 	if err != nil {
-		log.Fatal(utils.RED, err)
+		log.Printf("Error loading relation.json: %v", err)
 	}
 
-	log.Println(utils.GREEN, "Data loaded", utils.NONE)
+	log.Println("Data loaded")
 }
